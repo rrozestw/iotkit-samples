01583217a7f3f49e2eea85528f7d1ec95f75ede4
Marcin Makowski <marcin.makowski@intel.com>
2015-03-09 11:34:01 +0100

    improved examples

--- arduino/IoTkit/examples/IoTKitQuickStartExample/Setup.ino
+++ arduino/IoTkit/examples/IoTKitQuickStartExample/Setup.ino
@@ -2,13 +2,14 @@
 #include <IoTkit.h>
 #include <Ethernet.h>
 #include <aJSON.h>
-#include <rgb_lcd.h>
+#include <rgb_lcd.h>  //library from https://github.com/Seeed-Studio/Sketchbook_Starter_Kit_V2.0/tree/master/libraries/Grove_LCD_RGB_Backlight
 #include <Wire.h>
+#include <stdio.h>
 
-char const * networkSSID="CHANGE_ME";
-char const * WIFI_identity="CHANGE_ME";
-char const *  WIFI_password="CHANGE_ME";
-char const * activationCode="CHANGE_ME";
+char  * networkSSID="CHANGE_ME";
+char  * WIFI_identity="CHANGE_ME";
+char  * WIFI_password="CHANGE_ME";
+char  * activationCode="CHANGE_ME";
 
 IoTkit iotkit;
 int inPinTemperatureSensor = A0;
@@ -16,197 +17,240 @@ int outPinLed = 3;
 rgb_lcd lcd;
 
 float TemperatureValue;
-int B=3975;                  //B value of the thermistor
-float resistance;
+float minTempBlue = 22;//0% value
+float maxTempRed = 40;//100% value
 
 void setup() {
-  Serial.begin(115200);
-  iotkit.begin();
-  delay(1000);
-  if(network_not_set()){
-     setNetwork();
-  }
-  if(device_not_activated()){
-   activateAndRegisterComponents();
-  }
+    Serial.begin(115200);
+    iotkit.begin();
+    pinMode(outPinLed, OUTPUT);
+    lcd.begin(16, 2);
+    lcd.setRGB(0,174,239);
+    ShowInfoLog("Intel Inside","Edison");
+    delay(5000);
 
-   pinMode(outPinLed, OUTPUT);
-  lcd.begin(16, 2);
+    if(network_not_set()){
+        setNetwork();
+    }
+    if(device_not_activated()){
+        activateAndRegisterComponents();
+    }
+
 }
 
 void loop() {
- readAllSensors();
- turnOnOfLed();
- Display();
- iotkit.send("temperature", TemperatureValue);
- delay(1000);
+    readAllSensors();
+    turnOnOfLed();
+    Display();
+    iotkit.send("temperature", TemperatureValue);
+    delay(1000);
 }
 
 void readAllSensors(){
-  int temp =  analogRead(inPinTemperatureSensor);
-  resistance=(float)(1023-temp)*10000/temp; //get the resistance of the sensor;
-  TemperatureValue=1/(log(resistance/10000)/B+1/298.15)-273.15;//convert to temperature via datasheet ;
+    //Read temperature sensor
+    int temp =  analogRead(inPinTemperatureSensor);
+    float resistance = (float)(1023-temp)*10000/temp;
+    TemperatureValue= 1/(log(resistance/10000)/3975+1/298.15)-273.15;
 }
 
 void turnOnOfLed(){
- if( TemperatureValue > 36.6){
-    digitalWrite(outPinLed, HIGH);
- } else {
-   digitalWrite(outPinLed, LOW);
- }
+    if( TemperatureValue > maxTempRed){
+        digitalWrite(outPinLed, HIGH);
+    } else {
+        digitalWrite(outPinLed, LOW);
+    }
 }
 
 void lcdClearRow(int row){
-  lcd.setCursor(0,row);
-  lcd.print("                ");
+    lcd.setCursor(0,row);
+    lcd.print("                ");
 }
 
-float minTempBlue = 20;//0% value
-float maxTempRed = 50;//100% value
+
 void Display(){
-  //from blue to red
-  int tempProcentValue = (TemperatureValue - minTempBlue) * (100 / (maxTempRed-minTempBlue));
-  lcd.setRGB(255*tempProcentValue/100,0,(255 * (100 - tempProcentValue))/100);
+    //from blue to red
+    int tempProcentValue = (TemperatureValue - minTempBlue) * (100 / (maxTempRed-minTempBlue));
+    lcd.setRGB(255*tempProcentValue/100,0,(255 * (100 - tempProcentValue))/100);
 
-  lcdClearRow(0);
-  lcd.setCursor(0,0);
-  lcd.print("Temperature:");
-  char* formated = new char[512];
-  sprintf(formated, "%.2f C",TemperatureValue);
-  lcd.setCursor(0,1);
-  lcd.print(String(formated));
+    char* formated = new char[512];
+    sprintf(formated, "%2.2f C", TemperatureValue);
 
+    ShowInfoLog("Temperature:",formated);
+}
+
+void ShowInfoLog(char * firstLine, char* secondLine){
+    lcdClearRow(0);
+    lcdClearRow(1);
+    Serial.print(firstLine);
+    Serial.println(secondLine);
+    lcd.setCursor(0,0);
+    lcd.print(firstLine);
+    lcd.setCursor(0,1);
+    lcd.print(secondLine);
 }
 
 boolean device_not_activated(){
-  FILE *scans  = popen("iotkit-admin -V" ,"r");
-  char buffer[6];
-  char *line_p = fgets(buffer, sizeof(buffer), scans);
-  Serial.println("Agent version");
-  Serial.println(buffer);
-  if(buffer[0]=='1'){
-    if(buffer[2]=='5'){
 
-     FILE *grep  = popen( "grep 'deviceToken' /usr/share/iotkit-agent/certs/token.json | awk '/,/{gsub(/ /,\"\",$0);gsub(/\"deviceToken\":/,\"\",$0);print $0;}'","r");
-     fgets(buffer, sizeof(buffer), grep);
-     Serial.println(buffer);
-     if(strcmp(buffer,"false")==0){
-        Serial.println("Token not set");
-       return true;
-     }
+    char* agentVersion = getProcessOutput("iotkit-admin -V");
+    ShowInfoLog("Agent version", agentVersion);
+
+    if(agentVersion[0]=='1'){
+        if(agentVersion[2]=='5'){
+            //Edison iotki-agent 1.5.0 tokens location are in /usr/share/iotkit-agent
+            char* tokenIsSet  = getProcessOutput("grep 'deviceToken' /usr/share/iotkit-agent/certs/token.json | awk '/,/{gsub(/ /,\"\",$0);gsub(/\"deviceToken\":/,\"\",$0);print $0;}'");
+            if(strstr(tokenIsSet, "false") != NULL){
+                ShowInfoLog("Token not set","");
+                return true;
+            }
+        }
     }
-  }
-  Serial.println("Token set");
-  return false;
+    ShowInfoLog("Token set","");
+    return false;
 }
+
 void activateAndRegisterComponents(){
 
-  char* formated = new char[512];
-  sprintf(formated, "iotkit-admin activate %s > /dev/ttyGS0 2>&1", activationCode);
-  Serial.println(formated);
-  system(formated);
-  system("iotkit-admin register temperature temperature.v1.0 > /dev/ttyGS0 2>&1");
-  system("systemctl start iotkit-agent");
-  system("systemctl enable iotkit-agent");
+    char* formated = new char[512];
+    sprintf(formated, "iotkit-admin activate %s > /dev/ttyGS0 2>&1", activationCode);
+    ShowInfoLog("Activating with code", activationCode);
+    system(formated);
+
+    if(device_not_activated()){
+        lcd.setRGB(255,0,0);
+        ShowInfoLog("There was error","during activation");
+    }
+    else {
+        lcd.setRGB(0,255,0);
+        ShowInfoLog("Activation","completed.");
+        system("iotkit-admin register temperature temperature.v1.0 > /dev/ttyGS0 2>&1");
+        system("iotkit-admin register powerswitch powerswitch.v1.0 > /dev/ttyGS0 2>&1"); //for future actution
+        system("systemctl start iotkit-agent");
+        system("systemctl enable iotkit-agent");
+    }
+    delay(2500);
 }
 
 boolean network_not_set(){
-  FILE *scans  = popen("wpa_cli status | grep ^ssid | awk '{split($0,s,\"=\");print s[2]}' " ,"r");
-  if(scans){
-     char buffer[strlen(networkSSID)+1];
-     char *line_p = fgets(buffer, sizeof(buffer), scans);
 
-     if(strcmp(buffer,networkSSID) == 0){
-       return false;
-     }
-  }
-  Serial.println("Network not set");
-  return true;
+    char* isSet = getProcessOutput("wpa_cli status | grep ^ssid | awk '{split($0,s,\"=\");print s[2]}'");
+
+    if(isSet){
+        if(strstr(isSet, networkSSID) != NULL){
+            ShowInfoLog("Network is set", networkSSID);
+            return false;
+        }
+    }
+    ShowInfoLog(networkSSID, "network not set");
+    return true;
 }
 
 String scanNetwork(){
-  system("systemctl stop hostapd && sleep 2 && systemctl start wpa_supplicant");
+    system("systemctl stop hostapd && sleep 2 && systemctl start wpa_supplicant");
+    ShowInfoLog("Scanning network...","");
+    for(int i=0;i<6;i++){
+        system("wpa_cli scan");
+        delay(1000);
+    }
+    char* formated = new char[512];
+    sprintf(formated, "wpa_cli scan_results | grep %s | awk 'NR==1{gsub(/\\[/,\"\",$0]);gsub(/\\]/,\"\",$0);print $4}'", networkSSID);
 
-  for(int i=0;i<6;i++){
-    system("wpa_cli scan");
-    delay(1000);
-  }
-  char* formated = new char[512];
-  sprintf(formated, "wpa_cli scan_results | grep %s | awk 'NR==1{print $4}'",networkSSID);
-  Serial.println(formated);
-  FILE *scans  = popen(formated ,"r");
+    char* scanResult = getProcessOutput(formated);
 
-  if (scans)
-  {
-    char buffer[1024];
-    char *line_p = fgets(buffer, sizeof(buffer), scans);
+    if (scanResult){
 
-    Serial.print(buffer);
-    if(buffer[1]=='W' && buffer[2]=='E'){return "WEP";}
-        if(buffer[1]=='O'){return "OPEN";}
-         if(buffer[1]=='W' && buffer[2]=='P'){
-           if(buffer[4]=='2'){
-             if(buffer[6]=='E')
-             return "EAP";
-             return "PSK";
-           }
-           if(buffer[5]=='E')
-           return "EAP";
-           return "PSK";
-         }
-  }
-  return "";
+        ShowInfoLog("Network type:",scanResult);
+        if(strstr(scanResult,"WPA2-EAP")!= NULL || strstr(scanResult,"WPA-EAP")!=NULL){
+        return "EAP";
+        } else if(strstr(scanResult,"WEP")!=NULL){
+        return "WEP";
+        } else if(strstr(scanResult,"OPEN")!=NULL){
+        return "OPEN";
+        }else if(strstr(scanResult,"WPA-PSK")!=NULL){
+        return "PSK";
+        }
+    }
+
+    return "";
+}
+
+void WriteWpaConfig(){
+    String networkType = scanNetwork();
+
+
+    char * network_conf = new char[512];
+
+
+    if(networkType== "EAP"){
+        sprintf(network_conf,"network={\nssid=\"%s\"\n\nkey_mgmt=WPA-EAP\npairwise=CCMP TKIP\ngroup=CCMP TKIP WEP104 WEP40\neap=TTLS PEAP TLS\nidentity=\"%s\"\npassword=\"%s\"\nphase1=\"peaplabel=0\"\n}\n",networkSSID,WIFI_identity,WIFI_password);
+    }
+    if(networkType== "OPEN"){
+        sprintf(network_conf,"network={\nssid=\"%s\"\n\nkey_mgmt=NONE\n}\n",networkSSID);
+    }
+    if(networkType== "PSK"){
+        sprintf(network_conf,"network={\nssid=\"%s\"\n\nkey_mgmt=WPA-PSK\npairwise=CCMP TKIP\ngroup=CCMP TKIP WEP104 WEP40\neap=TTLS PEAP TLS\npsk=\"%s\"\n}\n",networkSSID,WIFI_password);
+    }
+    if(networkType== "WEP"){
+        sprintf(network_conf,"network={\nssid=\"%s\"\n\nkey_mgmt=NONE\ngroup=WEP104 WEP40\nwep_key0=\"%s\"\n}\n",networkSSID,WIFI_password);
+    }
+
+    //write configuration to file
+    FILE *file =  fopen("/etc/wpa_supplicant/wpa_supplicant.conf","a");
+    fwrite(network_conf2, sizeof(char),len,file);
+    fclose(file);
+}
+
+void RestartNetworking(){
+    ShowInfoLog("Restarting network","");
+    system("systemctl stop hostapd");
+    system("systemctl restart wpa_supplicant");
+    system("systemctl enable wpa_supplicant");
+}
+
+void SelectNetwork(){
+    char* numOfNetworks = getProcessOutput("wpa_cli list_networks | awk 'END{print NR - 3}'");
+    char * networkSelect = new char[512];
+    sprintf(networkSelect, "wpa_cli select_network %s", numOfNetworks);
+    system(networkSelect);
+}
+
+void WaitForIp(int timeout){
+    for(int i = 0;i < timeout;i++){
+        char* timeLeft = new char[15];
+        sprintf(timeLeft,"%i seconds left", (30-i));
+        ShowInfoLog("Waiting for ip address...", timeLeft);
+        char* ip = getProcessOutput("wpa_cli status | grep ^ip_address | awk '{split($0,s,\"=\");print s[2]}'");
+        if(String(ip) != ""){
+            ShowInfoLog("ip address: ", ip);
+            break;
+        }
+        delay(1000);
+    }
 }
 
 void setNetwork(){
     String networkType = scanNetwork();
-
-    int len = 0;
-    char * network_conf2 = new char[512];
     if(networkType!=""){
-
-       if(networkType== "EAP"){
-          len = sprintf(network_conf2,"network={\nssid=\"%s\"\n\nkey_mgmt=WPA-EAP\npairwise=CCMP TKIP\ngroup=CCMP TKIP WEP104 WEP40\neap=TTLS PEAP TLS\nidentity=\"%s\"\npassword=\"%s\"\nphase1=\"peaplabel=0\"\n}\n",networkSSID,WIFI_identity,WIFI_password);
-       }
-       if(networkType== "OPEN"){
-       len = sprintf(network_conf2,"network={\nssid=\"%s\"\n\nkey_mgmt=NONE\n}\n",networkSSID);
-       }
-       if(networkType== "PSK"){
-       len = sprintf(network_conf2,"network={\nssid=\"%s\"\n\nkey_mgmt=WPA-PSK\npairwise=CCMP TKIP\ngroup=CCMP TKIP WEP104 WEP40\neap=TTLS PEAP TLS\npsk=\"%s\"\n}\n",networkSSID,WIFI_password);
-       }
-       if(networkType== "WEP"){
-       len = sprintf(network_conf2,"network={\nssid=\"%s\"\n\nkey_mgmt=NONE\ngroup=WEP104 WEP40\nwep_key0=\"%s\"\n}\n",networkSSID,WIFI_password);
-       }
-
-
-   FILE *file =  fopen("/etc/wpa_supplicant/wpa_supplicant.conf","a");
-
-
-   fwrite(network_conf2,sizeof(char),len,file);
-
-   fclose(file);
-   Serial.print("Restarting network");
-   system("systemctl stop hostapd");
-   system("systemctl restart wpa_supplicant");
-   system("systemctl enable wpa_supplicant");
-   system("wpa_cli select_network 0");
-   //we wait max 30s for
-   for(int i=0;i<30;i++){
-      FILE *scans  = popen("wpa_cli status | grep ^ip_address | awk '{split($0,s,\"=\");print s[2]}' " ,"r");
-      if(scans){
-         char buffer[1];
-         fgets(buffer, sizeof(buffer), scans);
-         if(String(buffer)!=""){
-           break;
-         }
-      }
-      delay(1000);
-   }
+        WriteWpaConfig();
+        RestartNetworking();
+        delay(1000);
+        SelectNetwork();
+        //we wait max 30s for
+        WaitForIp(30);
 
     } else {
-      Serial.print("Network type not detected!");
+        ShowInfoLog("Network not detected","");
     }
 
 }
 
 
+char* getProcessOutput(char* command){
+    FILE *out  = popen(command ,"r");
+    if(out){
+        char buffer[512];
+        buffer[0] = '\0';
+        fgets(buffer, sizeof(buffer), out);
+        return buffer;
+    }
+    return "";
+}
